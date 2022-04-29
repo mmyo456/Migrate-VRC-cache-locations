8b41f0123f6e261b23556c9559f05357ca00680d
Mingming8886 <86056701+Mingming8886@users.noreply.github.com>
2022-04-29 21:06:17 +0800

    Update emmm.bat

--- emmm.bat
+++ emmm.bat
@@ -1,40 +1,44 @@
 @echo off
+title 自定义VRC缓存目录(禁止倒卖
 echo ------
 echo 摆！
 echo ------
+echo 作者博客：https://www.mmfo.top/
 echo Github：https://github.com/Mingming8886/Migrate-VRC-cache-locations
 echo 注意:该操作会删除VRC的所有缓存
-echo  之前加载过的物品都需重新加载
+echo 之前加载过的物品都需重新加载
 echo PS：如您是刚下载VRCHAT麻烦您启动一次再来操作
-echo 是否操作？回车=确认，关闭程序=取消
+echo 版本号V: 1.4
+tasklist | find /i "VRChat.exe" >nul 2>nul && echo [日志] 检测到您的VRChat正在运行，请关闭再来操作！ || echo [日志] 未检测到你的VRChat请继续操作
 PAUSE
 net session >nul 2>&1
 if %errorLevel% == 0 (
     rem Success: [日志] 确认管理员权限成功!
 ) else (
-    echo [日志] 程序无管理员操作，取消所有操作!
-    echo        操作已被取消!
+cls
+    echo [日志] 无管理员操作权限，已取消操作(错误代码001
+    echo [日志] 已取消运行
     echo.
-    echo [日志]:  请右键以管理员身份运行该脚本！
+    echo [日志] 解决方案：请右键以管理员身份运行该脚本！
     echo.
     pause
     exit
 )
 
-
-set /p dpath=请输入目的路径（例如：D:\Temp ，工具会自动在你输入的路径下创建VRChat_Cache文件夹并建立软连接）:
+cls
+set /p dpath=请输入目的路径（例如：E:\VRChat ，程序会自动在你输入的路径下创建VRChat_Cache文件夹并建立软连接）:
 echo 你输入的路径是 %dpath% 
 pause
 md %dpath%\VRChat_Cache
-echo 复制文件......
+echo [日志] 复制文件
 echo=
 xcopy %APPDATA%\..\LocalLow\VRChat %dpath%\VRChat_Cache /s /e /g /h
-echo 删除原目录
+echo [日志] 删除原目录
 echo=
 rd /s /q %APPDATA%\..\LocalLow\VRChat
-echo 创建软连接
+echo [日志] 创建软连接
 echo=
 mklink /d %APPDATA%\..\LocalLow\VRChat %dpath%\VRChat_Cache
-echo 缓存转移完成
-echo=
+echo [日志] 缓存转移完成
+echo
 pause
